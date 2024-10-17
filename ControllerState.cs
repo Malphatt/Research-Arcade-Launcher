41cc9e3f62e085927c9f59845b8e70c93e6432a2
Malphatt <38288537+Malphatt@users.noreply.github.com>
2024-10-17 11:25:11 +0100

    V1.6.0-24 Release

---
+++ ControllerState.cs
@@ -0,0 +1,121 @@
+﻿using SharpDX.DirectInput;
+
+namespace ArcademiaGameLauncher
+{
+    internal class ControllerState
+    {
+        private readonly int index;
+        private readonly bool[] buttonStates;
+        private readonly bool[] buttonDownStates;
+        private int leftStickX;
+        private int leftStickY;
+
+        // Deadzone and Midpoint values for the joystick
+        readonly int joystickDeadzone = 7700;
+        readonly int joystickMidpoint = 32511;
+
+        public Joystick joystick;
+        public JoystickState state;
+
+        public ControllerState(Joystick _joystick, int _index)
+        {
+            // Set the index of the controller
+            index = _index;
+            // Set the joystick
+            joystick = _joystick;
+
+            // Initialize the button states
+            state = new JoystickState();
+            buttonStates = new bool[128];
+            buttonDownStates = new bool[128];
+            state = joystick.GetCurrentState();
+        }
+
+        public void UpdateButtonStates()
+        {
+            // Poll the joystick for the current state
+            joystick.Poll();
+            state = joystick.GetCurrentState();
+
+
+            // Update the joystick states
+            leftStickX = state.X;
+            leftStickY = state.Y;
+
+            // Update the button states
+            for (int i = 0; i < buttonStates.Length; i++)
+            {
+                SetButtonState(i, state.Buttons[i]);
+            }
+        }
+
+        // Getters for the joystick states
+        public int GetLeftStickX()
+        {
+            return leftStickX;
+        }
+        public int GetLeftStickY()
+        {
+            return leftStickY;
+        }
+
+        // Getters for the joystick directions
+        public int[] GetLeftStickDirection()
+        {
+            int[] direction = new int[2];
+
+            if (leftStickX > joystickMidpoint + joystickDeadzone)
+            {
+                direction[0] = 1;
+            }
+            else if (leftStickX < joystickMidpoint - joystickDeadzone)
+            {
+                direction[0] = -1;
+            }
+            else
+            {
+                direction[0] = 0;
+            }
+
+            if (leftStickY > joystickMidpoint + joystickDeadzone)
+            {
+                direction[1] = 1;
+            }
+            else if (leftStickY < joystickMidpoint - joystickDeadzone)
+            {
+                direction[1] = -1;
+            }
+            else
+            {
+                direction[1] = 0;
+            }
+
+            return direction;
+        }
+
+        // Getter and Setter for the button states
+        public bool GetButtonState(int _button)
+        {
+            return buttonStates[_button];
+        }
+        public bool GetButtonDownState(int _button)
+        {
+            return buttonDownStates[_button];
+        }
+        public void SetButtonState(int _button, bool _buttonState)
+        {
+            if (!buttonStates[_button] && _buttonState)
+                buttonDownStates[_button] = true;
+            else
+                buttonDownStates[_button] = false;
+
+            buttonStates[_button] = _buttonState;
+        }
+
+        // Getter for the index
+        public int GetIndex()
+        {
+            return index;
+        }
+    }
+}
---
+++ Socket.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using SocketIOClient;
+
+
+namespace ArcademiaGameLauncher
+{
+    internal class Socket
+    {
+        readonly MainWindow mainWindow;
+        readonly SocketIOClient.SocketIO client;
+        private readonly string clientName;
+
+        public Socket(string ip, string port, string clientName, MainWindow mainWindow)
+        {
+            Console.WriteLine("Connecting to " + ip + ":" + port);
+
+            this.mainWindow = mainWindow;
+            client = new SocketIOClient.SocketIO("ws://" + ip + ":" + port, new SocketIOOptions()
+            {
+                Reconnection = true,
+                ReconnectionAttempts = 1,
+                ReconnectionDelay = 1000,
+                ReconnectionDelayMax = 5000,
+                ConnectionTimeout = TimeSpan.FromSeconds(5)
+            });
+            this.clientName = clientName;
+
+            HandleEmits();
+            Connect();
+        }
+
+        private async void Connect() => await client.ConnectAsync();
+
+        private void HandleEmits()
+        {
+            client.OnConnected += Socket_OnConnected;
+            client.OnDisconnected += Socket_OnDisconnected;
+            client.OnError += Socket_OnError;
+            client.OnReconnected += Socket_OnReconnected;
+            client.OnReconnectAttempt += Socket_OnReconnecting;
+            client.OnReconnectFailed += Socket_OnReconnectFailed;
+
+            client.On("PlayAudio", response => Socket_PlayAudio(response));
+        }
+
+        private void Socket_OnConnected(object sender, EventArgs e)
+        {
+            Console.WriteLine("[Socket] Connected");
+
+            client.EmitAsync("register", clientName);
+        }
+
+        private void Socket_OnDisconnected(object sender, string e)
+        {
+            Console.WriteLine("[Socket] Disconnected");
+        }
+
+        private void Socket_OnError(object sender, string e)
+        {
+            Console.WriteLine("[Socket] Error: " + e);
+        }
+
+        private void Socket_OnReconnected(object sender, int e)
+        {
+            Console.WriteLine("[Socket] Reconnected");
+        }
+
+        private void Socket_OnReconnecting(object sender, int e)
+        {
+            Console.WriteLine("[Socket] Reconnecting");
+        }
+
+        private void Socket_OnReconnectFailed(object sender, EventArgs e)
+        {
+            Console.WriteLine("[Socket] Reconnect failed");
+        }
+
+        private void Socket_PlayAudio(SocketIOResponse response)
+        {
+            Console.WriteLine("[Socket] PlayAudio: " + response.GetValue<string>());
+            mainWindow.PlayAudioFile(response.GetValue<string>());
+        }
+    }
+}
